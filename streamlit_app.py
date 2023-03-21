12c9771d5cb4d306f5f3d09c0a8a1968d0142f3d
steph575757 <128503390+steph575757@users.noreply.github.com>
2023-03-21 17:03:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner');
+
 streamlit.header('Breakfast Menu');
+
 streamlit.text('Omega 3 and Blueberry Oatmeal');
 streamlit.text('Kale, Spinach and Rocket Smoothie');
 streamlit.text('Hard-Boiled Free-Range Egg');
